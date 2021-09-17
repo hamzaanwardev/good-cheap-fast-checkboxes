3e3f2b86c3e32e49fc86a7b687990a03d5dfb355
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-17 23:53:47 +0500

    completed

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
-var toggles = document.querySelectorAll('.toggle');
-var good = document.getElementById('good');
-var cheap = document.getElementById('cheap');
-var fast = document.getElementById('fast');
+// const toggles = document.querySelectorAll('.toggle');
+// const good = document.getElementById('good');
+// const cheap = document.getElementById('cheap');
+// const fast = document.getElementById('fast');
 
-toggles.forEach(toggle => toggle.addEventListener ('change', (e) => doTheTrick(e.target)))
+// toggles.forEach(toggle => toggle.addEventListener ('change', (e) => doTheTrick (e.target)))
 
-function doTheTrick (theClickedOne) {
-    if (good.checked && cheap.checked && fast.checked) {
-        if (good === theClickedOne) {
+// function doTheTrick (theClickedOne) {
+//     if (good.checked && cheap.checked && fast.checked) {
+//         if (good === theClickedOne) {
+//             fast.checked = false
+//         }
+//         if (cheap === theClickedOne) {
+//             good.checked = false
+//         }
+//         if (fast === theClickedOne) {
+//             cheap.checked = false
+//         }
+//     }
+// }
+
+
+
+
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickedOne) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === theClickedOne) {
             fast.checked = false
         }
-        if (cheap === theClickedOne) {
+
+        if(cheap === theClickedOne) {
             good.checked = false
         }
-        if (fast === theClickedOne) {
+
+        if(fast === theClickedOne) {
             cheap.checked = false
         }
     }
